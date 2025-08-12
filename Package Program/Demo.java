3e34e18c30e4f3073cf3af552448b0e9f86aa0b2
rms-r7 <rikashleo@gmail.com>
2025-08-12 20:31:03 +0530

    package

---
+++ Package Program/Demo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo {
+    public String message = "Hello from Demo!";
+
+    public void displayMessage() {
+        System.out.println("Calling from outside the 'demo' package.");
+    }
+}
